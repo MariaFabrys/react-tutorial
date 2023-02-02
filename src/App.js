805a7abc8417f88a799e1545e1b3cd18d1ee17b8
MariaFabrys <mariaeduardafabris12@gmail.com>
2023-02-02 17:57:06 -0300

    Add Hello world

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Herzlich willkommen</h1>
+          Hello world React! <br></br>
+          <i>Olá mundo React!</i> <br></br>
+         <b>Ciao mondo React!</b> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
