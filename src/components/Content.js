e32b1a419e566b9998d1628abc41520fad7af4b0
Maria Fabris <mariaeduardafabris12@gmail.com>
2023-03-19 12:59:35 -0300

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,6 @@
 import Box from '@mui/material/Box'
 import Alert from './Alert'
-import photoBunny from './assets/img/coelhoOrelhudo.webp'
-import { ReactComponent as IconCarrinho} from './assets/img/carrinho.svg'
 
-import {GiStarfighter as IconStar} from 'react-icons/gi'
 import SampleUseState from './SampleUseState'
 
 
@@ -32,24 +29,10 @@ const Content = () => {
 
             <SampleUseState/>
 
-            <img
-              style={{width: 500, height: 333.23}}
-             src='https://v5j9q4b5.rocketcdn.me/wp-content/uploads/2021/04/coelhos-principais-caracteristicas-e-como-cuidar-dentro-de-casa-960x640.jpg.webp'
-              alt='Bunny'>
-            </img>
             
-            <img
-              style={{width: 500, height: 333.23}}
-              src={photoBunny} 
-              alt='Bunny2'>
-            </img>
-
             <br/>
 
-            {/* ReactComponent */}
-            <IconCarrinho style={{width: 30, height: 30}}/>
 
-            <IconStar style={{width: 40, height: 30, color:'#fff'}}/>
         </Box>
     )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import './Sidebar.css'
 import Box from '@mui/material/Box';
+import photoBunny from './assets/img/coelhoOrelhudo.webp'
+import { ReactComponent as IconCarrinho} from './assets/img/carrinho.svg'
+
+import {GiStarfighter as IconStar} from 'react-icons/gi'
+
 
 
 const Sidebar = () => {
@@ -11,8 +16,28 @@ const Sidebar = () => {
                 <li>Comunidade</li>
                 <li>Sobre</li>
             </ul>
+        <div class="img-coelhos">
+        <img
+              style={{width: 150}}
+             src='https://v5j9q4b5.rocketcdn.me/wp-content/uploads/2021/04/coelhos-principais-caracteristicas-e-como-cuidar-dentro-de-casa-960x640.jpg.webp'
+              alt='Bunny'>
+            </img>
+            <hr/>
+            <img
+              style={{width: 150}}
+              src={photoBunny} 
+              alt='Bunny2'>
+            </img>
+        </div>
 
-            
+        {/* ReactComponent */}
+        <div class="icon">
+
+            <IconCarrinho style={{width: 30, height: 30}}/>
+
+            <IconStar style={{width: 40, height: 30, color:'#fff'}}/>
+        </div>
+ 
         </Box>
     )
 }
