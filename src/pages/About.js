1a0cc2bbee0d1c29a97bca10af7ff65f283d4738
Maria Fabris <mariaeduardafabris12@gmail.com>
2023-03-23 17:21:17 -0300

    create page about

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import Box from '@mui/material/Box'
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
+
+
+const About = () => {
+  return (
+    <>
+      <Header />
+      <Menu/>
+      <Box sx={{
+        display: 'flex',
+      }}>
+        <Sidebar />
+        <Box>
+            <h1>Quem Somos?</h1>
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  )
+}
+
+export default About
